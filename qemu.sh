4f784e22a0a94cae3f92e41aa9ea2d5a321518c4
Wagner Riffel <wgrriffel@gmail.com>
2018-12-08 04:02:46 -0200

    qemu: use clearner syntax

--- qemu.sh
+++ qemu.sh
@@ -5,13 +5,9 @@ CDROOT=$TMP/cdroot
 
 mkdir -p $CDROOT/boot/grub
 cp kern $CDROOT/boot
-cat > $CDROOT/boot/grub/grub.cfg << EOF
-set timeout=1
+echo -n 'set timeout=1 menuentry "kern" --class os { multiboot (hd96)/boot/kern }'\
+> $CDROOT/boot/grub/grub.cfg
 
-menuentry "kern" --class os {
-	multiboot (hd96)/boot/kern
-}
-EOF
 grub-mkrescue -o $TMP/g.iso $CDROOT
 
 qemu-system-x86_64 -enable-kvm -ctrl-grab -gdb tcp::1234 -m 64 -monitor stdio\
