9df5096a68a44560b2c253a80f9e1c8e8fdc67db
Wagner Riffel <wgrriffel@gmail.com>
2018-12-11 03:32:35 -0200

    print: move print to c

--- kern.c
+++ kern.c
@@ -1,14 +1,21 @@
 #include <u.h>
 #include <defs.h>
 
-#define CGA_ADDR 0xb8000
-
 /* TODO(wgr): get rid of these gcc extensions */
 /* GO AWAY, DO NOT MESS WITH SPACE ABOVE!!!! */
-uint8 stack[4096] __attribute__ ((aligned (4)));
+uint8 stack[_STACKSIZE] __attribute__ ((aligned (4)));
 
-extern void print(void);
+/* not safe, str size may overlap available memory */
+void print(byte *s) {
+	volatile uint16 *v;
+	v = (volatile uint16*)0xb8000;
+	while(*s){
+		*v = (0x7 << 8) | *s;
+		v++;
+		s++;
+	}
+}
+
 void main(void) {
-	print();
 	for(;;);
 }
