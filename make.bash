10513df832e4b00adc097aad9b518aecf1f517b5
Wagner Riffel <wgrriffel@gmail.com>
2018-12-17 05:43:23 -0200

    make: automatic detect c and assember files

--- make.bash
+++ make.bash
@@ -2,32 +2,28 @@
 
 CC=clang
 
-SRC=\
-'
-src/io/uart.c
-src/kern.c
-src/boot.S
-src/io/serport.S
-'
-
-OBJ='obj/boot.o obj/kern.o obj/io/serport.o obj/io/uart.o'
-
-CPPFLAGS=-nostdinc
-CFLAGS='-no-integrated-as -c -O0 -g -m32 -ffreestanding -fno-strict-aliasing '$CPPFLAGS
-LDFLAGS='
--melf_i386 -static -nostdlib
--T src/script.ld --build-id=none -o bin/kern
-'
-
+OBJ=`du -a . | grep '.*\.o$' | awk '{print $2}'`
 if [ "$1" = "clean" ]; then
 	echo rm -f $OBJ
 	rm -f $OBJ
 	exit 0
 fi
 
+SRC=`du -a . | grep '.*\.c$' | awk '{print $2}'`
+ASM=`du -a . | grep '.*\.s$' | awk '{print $2}'`
+
+CPPFLAGS='-nostdinc -I./include'
+CFLAGS='-c -O0 -g -m32 -ffreestanding '$CPPFLAGS
+LDFLAGS='-melf_i386 -static -nostdlib -T src/script.ld --build-id=none -o bin/kern'
+
 for s in $SRC; do
-	echo $CC $CFLAGS -o obj/`echo $s|sed "s/src\///g"|sed "s/[\.c|\.S]//g"`.o $s
-	$CC $CFLAGS -o obj/`echo $s|sed "s/src\///g"|sed "s/[\.c|\.S]//g"`.o $s || exit 1
+	echo $CC $CFLAGS -o obj/`echo $s|sed "s/src\///g"|sed "s/[\.c$]//g"`.o $s
+	$CC $CFLAGS -o obj/`echo $s|sed "s/src\///g"|sed "s/[\.c$]//g"`.o $s || exit 1
+done
+
+for s in $ASM; do
+	echo $CC -x assembler-with-cpp -o obj/`echo $s|sed "s/src\///g"|sed "s/[\.s$]//g"`.o $s
+	$CC -o -x=assembler-with-cpp -no-integrated-as obj/`echo $s|sed "s/src\///g"|sed "s/[\.s]//g"`.o $s || exit 1
 done
 
 echo 'ld' $LDFLAGS $OBJ
