718b7b243b14bac641a7ddda27a3e166ace03471
Wagner Riffel <wgrriffel@gmail.com>
2018-12-17 06:44:00 -0200

    make: optimize finding, add cpp flags to assembler

--- make.bash
+++ make.bash
@@ -2,19 +2,19 @@
 
 CC=clang
 
-OBJ=`du -a . | grep '\.o$' | awk '{print $2}'`
+OBJ=`du -a ./obj | grep '\.o$' | awk '{print $2}'`
 if [ "$1" = "clean" ]; then
 	echo rm -f $OBJ
 	rm -f $OBJ
 	exit 0
 fi
 
-SRC=`du -a . | grep '\.c$' | awk '{print $2}'`
-ASM=`du -a . | grep '\.s$' | awk '{print $2}'`
+SRC=`du -a ./src | grep '\.c$' | awk '{print $2}'`
+ASM=`du -a ./src | grep '\.s$' | awk '{print $2}'`
 
 CPPFLAGS='-nostdinc -I./include'
 CFLAGS='-c -O0 -g -m32 -ffreestanding '$CPPFLAGS
-LDFLAGS='-melf_i386 -static -nostdlib -T src/script.ld --build-id=none -o bin/kern'
+LDFLAGS='-melf_i386 -static -nostdlib -T src/script.ld --build-id=none -o bin/kern '
 
 for s in $SRC; do
 	echo $CC $CFLAGS -o obj/`echo $s|sed "s/\.\/src\///g"|sed "s/\.c$/\.o/g"` $s
@@ -22,8 +22,8 @@ for s in $SRC; do
 done
 
 for s in $ASM; do
-	echo $CC -c -m32 -no-integrated-as -x assembler-with-cpp -o obj/`echo $s|sed "s/\.\/src\///g"|sed "s/\.s$/\.o/g"` $s 
-	$CC -c -m32 -no-integrated-as -x assembler-with-cpp -o obj/`echo $s|sed "s/\.\/src\///g"|sed "s/\.s$/\.o/g"` $s || exit 1
+	echo $CC -c -m32 -no-integrated-as -x assembler-with-cpp -nostdinc -I./include -o obj/`echo $s|sed "s/\.\/src\///g"|sed "s/\.s$/\.o/g"` $s
+	$CC -c -m32 -no-integrated-as -x assembler-with-cpp -nostdinc -I./include -o obj/`echo $s|sed "s/\.\/src\///g"|sed "s/\.s$/\.o/g"` $s || exit 1
 done
 
 echo 'ld' $LDFLAGS $OBJ
